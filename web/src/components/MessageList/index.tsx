44656c654988dfe0b315ba974ee9d1046e17fe4e
mnishidaramos <mnishidaramos@gmail.com>
2022-03-12 17:22:35 -0300

    Finalizacao do Stage 2 - Web Socket e mais

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,4 +1,5 @@
 import styles from './styles.module.scss';
+import { io } from 'socket.io-client';
 import { api } from '../../services/api';
 import { useEffect, useState } from 'react';
 
@@ -11,9 +12,30 @@ type Message = {
     };
 }
 
+const messagesQueue: Message[] = [];
+
+const socket = io('http://localhost:4000');
+
+socket.on('new_message', newMessage => {
+    messagesQueue.push(newMessage);
+})
 
 export function MessageList() {
     const [messages, setMessages] = useState<Message[]>([]);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            if (messagesQueue.length > 0) {
+                setMessages(prevState => [
+                    messagesQueue[0],
+                    prevState[0],
+                    prevState[1],
+                ].filter(Boolean));
+
+                messagesQueue.shift();
+            }
+        }, 3000);
+    }, []);
 
 
     useEffect(() => {
